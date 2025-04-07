6ef2b38a54d14ecea26fa93a2c1d8cccbb71c316
Nicolas Hery <hery.nicolas@gmail.com>
2025-04-07 12:10:01 -0400

    Improve shutdown

--- server/server.go
+++ server/server.go
@@ -29,10 +29,16 @@ func RunServer(ctx context.Context, handler http.Handler, logger *slog.Logger, p
 
 	go func() {
 		<-ctx.Done()
+		logger.Info("shutting down")
 		srv.Shutdown(context.Background())
 	}()
 
 	logger.Info(fmt.Sprintf("listening on port %d", port))
 
-	return srv.ListenAndServe()
+	err := srv.ListenAndServe()
+	if err != nil && err != http.ErrServerClosed {
+		return err
+	}
+
+	return nil
 }
